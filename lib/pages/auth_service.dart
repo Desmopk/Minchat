cde87dbcbe9e4a89f53f9d403fef6e84f6592b5e
Desmopk <pranav.kumar2022@vitstudent.ac.in>
2024-07-02 19:01:47 +0530

    firebase auth

---
+++ lib/pages/auth_service.dart
@@ -0,0 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+
+  Future<User?> signInAnonymously() async {
+    UserCredential userCredential = await _auth.signInAnonymously();
+    User? user = userCredential.user;
+    if (user != null) {
+      await _db.collection('users').doc(user.uid).set({
+        'uid': user.uid,
+        'last_seen': DateTime.now(),
+      });
+    }
+    return user;
+  }
+
+  Stream<List<UserModel>> getUsers() {
+    return _db.collection('users').snapshots().map((snapshot) {
+      return snapshot.docs.map((doc) => UserModel.fromDocument(doc)).toList();
+    });
+  }
+}
+
+class UserModel {
+  final String uid;
+
+  UserModel({required this.uid});
+
+  factory UserModel.fromDocument(DocumentSnapshot doc) {
+    return UserModel(
+      uid: doc['uid'],
+    );
+  }
+}
